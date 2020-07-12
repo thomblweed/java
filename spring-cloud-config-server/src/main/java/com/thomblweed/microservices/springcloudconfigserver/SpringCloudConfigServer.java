311f5d05ff24d8e9203c6b26b23dd1cbbad4b441
Thomas Newman <thomblweed@gmail.com>
2020-07-12 16:23:34 +0100

    config server

---
+++ spring-cloud-config-server/src/main/java/com/thomblweed/microservices/springcloudconfigserver/SpringCloudConfigServer.java
@@ -0,0 +1,13 @@
+package com.thomblweed.microservices.springcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServer.class, args);
+	}
+
+}
