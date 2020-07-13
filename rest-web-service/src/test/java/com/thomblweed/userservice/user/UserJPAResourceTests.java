2d1e752fdde132de39544c24d7e5ffc0034ce699
Thomas Newman <thomblweed@gmail.com>
2020-07-13 22:13:47 +0100

    unit test added for getbyId

--- rest-web-service/src/test/java/com/thomblweed/userservice/user/UserJPAResourceTests.java
+++ rest-web-service/src/test/java/com/thomblweed/userservice/user/UserJPAResourceTests.java
@@ -1,6 +1,5 @@
 package com.thomblweed.userservice.user;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.platform.runner.JUnitPlatform;
@@ -9,10 +8,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Links;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
 @RunWith(JUnitPlatform.class)
@@ -26,7 +29,13 @@ class UserJPAResourceTests {
 
     @Test
     public void testGetAllUsers() {
-        Mockito.when(mockUserRepository.findAll()).thenReturn(mockUsers());
+        User user1 = new User(1, "Thomas", new Date());
+        User user2 = new User(2, "Jeff", new Date());
+        List<User> userList = new ArrayList<>();
+        userList.add(user1);
+        userList.add(user2);
+
+        Mockito.when(mockUserRepository.findAll()).thenReturn(userList);
 
         List<User> userResult = userJPAResource.getAllUsers();
 
@@ -34,13 +43,25 @@ class UserJPAResourceTests {
         assertThat(userResult.get(1).getName()).isEqualTo("Jeff");
     }
 
-    private List<User> mockUsers() {
-        User user1 = new User(1, "Thomas", new Date());
-        User user2 = new User(2, "Jeff", new Date());
-        List<User> userList = new ArrayList<>();
-        userList.add(user1);
-        userList.add(user2);
+    @Test
+    public void testGetUserById() throws Exception {
+        User user = new User(1, "Thomas", new Date());
+        Optional<User> optionalUser = Optional.of(user);
 
-        return userList;
+        Mockito.when(mockUserRepository.findById(1)).thenReturn(optionalUser);
+
+        EntityModel<User> expected = EntityModel.of(optionalUser.get());
+
+        EntityModel<User> response = userJPAResource.getUserById(1);
+
+        User responseUser = response.getContent();
+        Links responseLinks = response.getLinks();
+
+        assertThat(responseUser).isEqualTo(expected.getContent());
+        assertThat(responseUser.getName()).isEqualTo("Thomas");
+        assertThat(responseUser.getId()).isEqualTo(1);
+        assertThat(responseLinks.hasLink("all-users")).isTrue();
+        assertThat(responseLinks.getRequiredLink("all-users").getHref())
+                .isEqualTo("/users/jpa/getAll");
     }
 }
