6839399eb14cef0cbc74c12054e68bbf90423d92
Thomas Newman <thomblweed@gmail.com>
2020-12-02 20:08:43 +0000

    singleton exercise added

---
+++ patterns/src/main/java/com/thomblweed/patterns/singleton/exercise/SingletonExercise.java
@@ -0,0 +1,9 @@
+package com.thomblweed.patterns.singleton.exercise;
+
+import java.util.function.Supplier;
+
+class SingletonTester {
+  public static boolean isSingleton(Supplier<Object> func) {
+    return func.get() == func.get();
+  }
+}
