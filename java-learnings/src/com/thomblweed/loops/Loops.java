2d29dfc5c3eb72239213138f259fa7b3503a4b29
Thomas Newman <thomblweed@gmail.com>
2020-12-06 16:24:05 +0000

    loops package added

---
+++ java-learnings/src/com/thomblweed/loops/Loops.java
@@ -0,0 +1,62 @@
+package com.thomblweed.loops;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Loops {
+
+    public static void main(String[] args) {
+        boolean isPalindrome = isPalindrome(-123321);
+        System.out.println(isPalindrome);
+    }
+
+    public static boolean isPalindrome(int number) {
+        int reverse = 0;
+        int startNumber = number;
+
+        while (number != 0) {
+            int lastDigit = number % 10;
+            reverse = (reverse * 10) + lastDigit;
+            number = number / 10;
+        }
+
+        if (startNumber == reverse)
+            return true;
+
+        return false;
+    }
+
+    public static int sumFirstAndLastDigit(int number) {
+        if (number < 0)
+            return -1;
+
+        String numberString = String.valueOf(number);
+        int firstDigit = Integer.parseInt(numberString.substring(0, 1));
+        int lastDigit = number % 10;
+
+        return firstDigit + lastDigit;
+    }
+
+    public static int getEvenDigitSum(int number) {
+        if (number < 0)
+            return -1;
+
+        int response = 0;
+
+        List<Integer> evenNumbers = new ArrayList<>();
+
+        int temp = 0;
+        while (number > 0) {
+            temp = number % 10;
+            if ((temp % 2) == 0)
+                evenNumbers.add(temp);
+            number = number / 10;
+        }
+
+        for (Integer evenNumber : evenNumbers) {
+            response = response + evenNumber;
+        }
+
+        return response;
+    }
+}
